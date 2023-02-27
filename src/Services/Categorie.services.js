7ae4bb571d68bd10bb2a359b0bc330243edeb9c9
khadija Elhardi <elhardykhadija@gmail.com>
2023-02-27 11:35:31 +0100

    commit categorie service

--- src/Services/Categorie.services.js
+++ src/Services/Categorie.services.js
@@ -9,10 +9,13 @@ async function getById(id){
 async function updateCategorie(id,categorie){
     return await http.put("/categories/"+id,categorie);
 }
+async function addCategorie(categorie){
+    return await http.post("/categories",categorie);
+}
 async function deleteCategorie(id){
     return await http.delete("/categories",categorie);
 }
 
 
-const CategorieService ={getAll,getById,updateCategorie,deleteCategorie}
+const CategorieService ={getAll,getById,updateCategorie,addCategorie,deleteCategorie}
 export default CategorieService;
