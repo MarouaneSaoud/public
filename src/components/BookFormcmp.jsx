af9150a38a6a522fccd42d4bb1559ac8554e7e7d
MarouaneSaoud <marouanesaoud84@gmail.com>
2023-02-28 21:34:46 +0100

    modier le formulaire d'ajouter book

--- src/components/BookFormcmp.jsx
+++ src/components/BookFormcmp.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 export default function BookForm() {
   return (
     <div>
-      <div class="min-h-screen mx-20 p-6 flex items-center justify-center">
+      <div class="min-h-screen mx-24 p-6 flex items-center justify-center">
         <div class="container max-w-screen-lg mx-auto">
-          <div className=''>
+          <div>
 
 
             <div class="bg-white rounded  shadow-lg p-4 px-4 md:p-8 mb-6">
@@ -19,12 +19,12 @@ export default function BookForm() {
                   <div class="grid gap-4 gap-y-2 text-sm grid-cols-1 md:grid-cols-5">
                     <div class="md:col-span-5">
                       <label for="name"> Name</label>
-                      <input type="text" required name="name" id="name" class="h-10 border mt-1 rounded px-4 w-full bg-gray-50" />
+                      <input  type="text" required name="name" id="name" class="h-10 border mt-1 rounded px-4 w-full bg-gray-50" />
                     </div>
 
                     <div class="md:col-span-5">
                       <label for="description">description</label>
-                      <textarea type="text" required name="description" id="description" class="h-10 border mt-1 rounded px-4 w-full bg-gray-50" />
+                      <textarea type="text"required name="description" id="description" class="h-10 border mt-1 rounded px-4 w-full bg-gray-50" />
                     </div>
 
                     <div class="md:col-span-3">
@@ -40,7 +40,7 @@ export default function BookForm() {
                     <div class="md:col-span-2">
                       <label for="editeur">editeur</label>
                       <div class="h-10 bg-gray-50 flex border border-gray-200 rounded items-center mt-1">
-                        <input type="text" required name="editeur"   class="px-4 appearance-none outline-none text-gray-800 w-full bg-transparent" />
+                        <input name="editeur" requiredclass="px-4 appearance-none outline-none text-gray-800 w-full bg-transparent" />
 
                       </div>
                     </div>
@@ -63,7 +63,7 @@ export default function BookForm() {
                       <label for="category">category</label>
                       <div class="h-10 bg-gray-50 flex border border-gray-200 rounded items-center mt-1">
                         
-                        <select id="category" class="bg-gray-50 border text-gray-800 w-full bg-transparent border-gray-300  text-sm rounded-lg  block  p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white ">
+                        <select required id="category" class="bg-gray-50 border text-gray-800 w-full bg-transparent border-gray-300  text-sm rounded-lg  block  p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white ">
                           <option selected>Choose a country</option>
                           <option value="US">United States</option>
                           <option value="CA">Canada</option>
