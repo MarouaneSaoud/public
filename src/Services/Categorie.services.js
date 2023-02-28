4f49880db64c869c1fbbb3012aeb9d015ca6377d
Aya <driouichayaa@gmail.com>
2023-02-28 16:50:32 +0100

    Afficher les categories

--- src/Services/Categorie.services.js
+++ src/Services/Categorie.services.js
@@ -1,19 +1,19 @@
 import http from "./http.common";
 
 async function getAll() {
-    return await http.get("/categories");
+    return await http.get("/category");
 }
 async function getById(id){
-    return await http.get("/categories/"+id);
+    return await http.get("/category/"+id);
 }
 async function updateCategorie(id,categorie){
-    return await http.put("/categories/"+id,categorie);
+    return await http.put("/category/"+id,categorie);
 }
 async function addCategorie(categorie){
     return await http.post("/categories",categorie);
 }
 async function deleteCategorie(id){
-    return await http.delete("/categories",categorie);
+    return await http.delete("/category");
 }
 
 
--- src/components/CategoryTable.jsx
+++ src/components/CategoryTable.jsx
@@ -1,12 +1,39 @@
-import React from 'react'
+import React , {useState , useEffect} from 'react'
 import { FaPen , FaTrash} from "react-icons/fa";
+import CategorieService from '../Services/Categorie.services';
 import {Link} from 'react-router-dom';
 
 export default function CategoryTable() {
+  const [Categorie, setCategorie] = useState([]);
+  async function deleteCategory(id){
+    try{
+      let result = await CategorieService.deleteCategorie(id);
+      getCategory();
+      
+    }
+    catch(error){
+      console.log(error);
+    }
+  }
+    async function getCategory(){
+    try{
+      let result = await CategorieService.getAll();
+      setCategorie(result.data);
+      
+    }
+    catch(error){
+      console.log(error);
+    }
+  }
+  useEffect (() =>{
+        getCategory()
+  },[]);
+
   return (
     <div className="mr-3">
     <table class=" w-full border-collapse bg-white text-left text-sm text-gray-500">
       <thead class="bg-gray-50">
+      
         <tr>
           <th scope="col" class="px-6 w-28 py-4 font-medium text-gray-900">
             Name
@@ -18,24 +45,27 @@ export default function CategoryTable() {
         </tr>
       </thead>
       <tbody class="divide-y divide-gray-100 border-t border-gray-100">
+      {Categorie.map((val, key)=>
+     
         <tr class="hover:bg-gray-50 text-base">
          
           <td class="px-6 py-4">
-            Lorem
+             {val.name}
           </td>
-          <td class="px-6 py-4">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Assumenda eos labore maxime suscipit accusamus cupiditate provident dolorem obcaecati, enim blanditiis similique? Aliquam sint cum ipsum quis odio odit blanditiis aspernatur.</td>
+          <td class="px-6 py-4">{val.description}</td>
         
           <td class="px-6 py-4">
             <div class="flex justify-end gap-4">
               <Link  to="/">
                 <FaPen className='text-green-700 hover:text-green-400'/>
               </Link>
-              <Link  to="/">
+              <button onClick={(e)=>{deleteCategory(val._id)}}>
               <FaTrash className='text-red-700 hover:text-red-400'/>
-              </Link>
+              </button>
             </div>
           </td>
         </tr>
+        )}
         
       </tbody>
     </table>
