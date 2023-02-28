efea00376452b7476cf6fcef99ff8357761a6922
MarouaneSaoud <marouanesaoud84@gmail.com>
2023-02-28 21:19:48 +0100

    modifier book categorie

--- src/components/BookFormcmp.jsx
+++ src/components/BookFormcmp.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 export default function BookForm() {
   return (
     <div>
-      <div class="min-h-screen p-6flex items-center justify-center">
+      <div class="min-h-screen mx-20 p-6 flex items-center justify-center">
         <div class="container max-w-screen-lg mx-auto">
-          <div className='mt-14'>
+          <div className=''>
 
 
             <div class="bg-white rounded  shadow-lg p-4 px-4 md:p-8 mb-6">
@@ -19,28 +19,28 @@ export default function BookForm() {
                   <div class="grid gap-4 gap-y-2 text-sm grid-cols-1 md:grid-cols-5">
                     <div class="md:col-span-5">
                       <label for="name"> Name</label>
-                      <input type="text" name="name" id="name" class="h-10 border mt-1 rounded px-4 w-full bg-gray-50" />
+                      <input type="text" required name="name" id="name" class="h-10 border mt-1 rounded px-4 w-full bg-gray-50" />
                     </div>
 
                     <div class="md:col-span-5">
                       <label for="description">description</label>
-                      <input type="text" name="description" id="description" class="h-10 border mt-1 rounded px-4 w-full bg-gray-50" />
+                      <textarea type="text" required name="description" id="description" class="h-10 border mt-1 rounded px-4 w-full bg-gray-50" />
                     </div>
 
                     <div class="md:col-span-3">
                       <label for="isbn">isbn</label>
-                      <input type="text" name="isbn" id="isbn" class="h-10 border mt-1 rounded px-4 w-full bg-gray-50" />
+                      <input type="text" required name="isbn" id="isbn" class="h-10 border mt-1 rounded px-4 w-full bg-gray-50" />
                     </div>
 
                     <div class="md:col-span-2">
                       <label for="auteur">auteur</label>
-                      <input type="text" name="auteur" id="auteur" class="h-10 border mt-1 rounded px-4 w-full bg-gray-50" />
+                      <input type="text" required name="auteur" id="auteur" class="h-10 border mt-1 rounded px-4 w-full bg-gray-50" />
                     </div>
 
                     <div class="md:col-span-2">
                       <label for="editeur">editeur</label>
                       <div class="h-10 bg-gray-50 flex border border-gray-200 rounded items-center mt-1">
-                        <input name="editeur" id="editeur" class="px-4 appearance-none outline-none text-gray-800 w-full bg-transparent" />
+                        <input type="text" required name="editeur"   class="px-4 appearance-none outline-none text-gray-800 w-full bg-transparent" />
 
                       </div>
                     </div>
@@ -48,18 +48,18 @@ export default function BookForm() {
                     <div class="md:col-span-2">
                       <label for="date_publication">date de publication</label>
                       <div class="h-10 bg-gray-50 flex border border-gray-200 rounded items-center mt-1">
-                        <input name="date_publication" type="date" id="date_publication" class="px-4 appearance-none outline-none text-gray-800 w-full bg-transparent" />
+                        <input name="date_publication" required type="date" id="date_publication" class="px-4 appearance-none outline-none text-gray-800 w-full bg-transparent" />
 
                       </div>
                     </div>
 
                     <div class="md:col-span-1">
                       <label for="zipcode">price</label>
-                      <input type="number" name="price" id="price" class="transition-all flex items-center h-10 border mt-1 rounded px-4 w-full bg-gray-50" />
+                      <input type="number" required name="price" id="price" class="transition-all flex items-center h-10 border mt-1 rounded px-4 w-full bg-gray-50" />
                     </div>
 
 
-                    <div class="md:col-span-2">
+                    <div class="md:col-span-3">
                       <label for="category">category</label>
                       <div class="h-10 bg-gray-50 flex border border-gray-200 rounded items-center mt-1">
                         
@@ -73,7 +73,7 @@ export default function BookForm() {
                       </div>
                     </div>
 
-                    <div class="md:col-span-3">
+                    <div class="md:col-span-5">
                       <div className='mt-4'>
 
                         <div class="flex  items-center justify-start bg-grey-lighter">
@@ -91,7 +91,7 @@ export default function BookForm() {
                             <span class="mt-2 text-base leading-normal">
                               Book Image
                             </span>
-                            <input className="hidden" id="dropzone-file" type="file" required />
+                            <input  className="hidden" id="dropzone-file" type="file" required />
 
                           </label>
                         </div>
