7481209a377dd62b30fc0da9b7407ed6ed8eb71d
MarouaneSaoud <marouanesaoud84@gmail.com>
2023-03-03 00:21:14 +0100

    commit

---
+++ src/components/CategoryStoreCard.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+export default function CategoryStoreCard() {
+  return (
+    <div id="category">
+      <div class="px-20 pb-52">
+      <div class="text-center p-10">
+        <h1 class="font-bold text-4xl mb-4">Categories </h1>
+     
+      </div>
+        <div class="bg-white p-6 m-7 rounded-lg shadow-lg">
+          <h2 class="text-2xl font-bold mb-2 text-gray-800">
+            Card with no image
+          </h2>
+          <p class="text-gray-700">This is my cool new card!</p>
+        </div>
+        <div class="bg-white p-6 m-7 rounded-lg shadow-lg">
+          <h2 class="text-2xl font-bold mb-2 text-gray-800">
+            Card with no image
+          </h2>
+          <p class="text-gray-700">This is my cool new card!</p>
+        </div>
+        
+      </div>
+      
+    </div>
+  );
+}
